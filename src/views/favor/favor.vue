3416527584107724ba5ae9c46aa29bdd37d7adab
yyy <1739198965@qq.com>
2022-08-19 22:33:59 +0800

    提交项目

---
+++ src/views/favor/favor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="favor" ref="favorRef">
+    <h2>favor</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import useScroll from '@/hooks/useScroll';
+
+const favorRef = ref()
+
+useScroll(favorRef)
+
+</script>
+
+<style lang="less" scoped>
+</style>
